d8704fa7a9e234c93fe2cb5a950694c80cd7b4ab
nahye6559@naver.com <kimnahye@gimnahyeui-MacBookAir.local>
2023-05-15 16:46:12 +0900

    .

--- JavaScript_SampleCode/2회차/training01/index.js
+++ JavaScript_SampleCode/2회차/training01/index.js
@@ -1,19 +1,19 @@
-const plusButton = document.querySelector('button.plus');
-const minusButton = document.querySelector('button.minus');
+const plusButton = document.querySelector("button.plus");
+const minusButton = document.querySelector("button.minus");
 const value = document.querySelector(".value");
 
-plusButton.addEventListener('click', () => {
-    //plusButton 누르면?
-    var tmp = Number(value.innerText);
-    console.log(tmp);
-    value.innerText = tmp + 1
-})
+plusButton.addEventListener("click", () => {
+  //plusButton 누르면?
+  var tmp = Number(value.innerText);
+  console.log(tmp);
+  value.innerText = tmp + 1;
+});
 
-minusButton.addEventListener('click', () => {
-    //minusButton 누르면?
-    var tmp = Number(value.innerText);
-    console.log(tmp);
-    value.innerText = tmp - 1
-})
+minusButton.addEventListener("click", () => {
+  //minusButton 누르면?
+  var tmp = Number(value.innerText);
+  console.log(tmp);
+  value.innerText = tmp - 1;
+});
 
-console.log(typeof(value.innerText));
+console.log(typeof value.innerText);
